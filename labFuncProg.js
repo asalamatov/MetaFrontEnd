ebb46def2a61a9a80591318a27f5ab929fa20fe0
Azamat Salamatov <azamat.salamatov2004@gmail.com>
2023-04-13 11:01:43 -0500

    oop and fp

---
+++ labFuncProg.js
@@ -0,0 +1,9 @@
+function consoleStyler(color, background, fontSize, txt) {
+    var message = "%c" + txt;
+    var style = `color: ${color};`
+    style += `background: ${background};`
+    style += `font-size: ${fontSize};`
+    console.log(message, style)
+}
+
+consoleStyler('red', 'blue', '14', 'hello')
